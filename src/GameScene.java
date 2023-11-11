e4b1da2c34321b5307a3c54eba1c59058890c39d
Merveille <merveille.m@hotmail.fr>
2023-11-11 13:13:49 +0100

    V.1

---
+++ src/GameScene.java
@@ -0,0 +1,8 @@
+import javafx.scene.Group;
+import javafx.scene.Scene;
+
+public class GameScene extends Scene {
+    public GameScene(Group root, double width, double height, boolean depthBuffer) {
+        super(root, width, height, depthBuffer);
+    }
+}
