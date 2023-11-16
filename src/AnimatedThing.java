1d5c9a478ce94d00341ea8d621b4c37f5fb7579d
Merveille <merveille.m@hotmail.fr>
2023-11-16 15:29:18 +0100

    V.2

--- src/AnimatedThing.java
+++ src/AnimatedThing.java
@@ -3,24 +3,16 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public abstract class AnimatedThing {
-    private final double x;
-    private final double y;
     private final ImageView imageView;
     private final int attitude;
     private final int index;
-    private final int duration;
-    private final int maxIndex;
     private final double windowSize;
     private final double frameOffset;
 
     public AnimatedThing(double x, double y, int attitude, int index, int duration, int maxIndex,
                          double windowSize, double frameOffset, String fileName) {
-        this.x = x;
-        this.y = y;
         this.attitude = attitude;
         this.index = index;
-        this.duration = duration;
-        this.maxIndex = maxIndex;
         this.windowSize = windowSize;
         this.frameOffset = frameOffset;
 
